ade4c7064983b28a9de7d3a199f10dc7f32257e3
gregbufano <113220067+gregbufano@users.noreply.github.com>
2025-04-28 05:06:08 -0700

    Added assets, player movement, and camera
    added tilestet for level and background
    add controller and mover for player movement
    added cinemacamera to allow the camera to follow the player(Just need to adjust the area it shows)

---
+++ Assets/Scripts/Controllers/Controller.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Mover mover;
+
+    public float speed;
+
+    public float jumpForce;
+
+    
+    public KeyCode moveLeft;
+    public KeyCode moveRight;
+    public KeyCode jump;
+    public KeyCode hit;
+
+    public bool grounded;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(moveLeft))
+        {
+            mover.MoveLeft(speed);
+        }
+        if (Input.GetKey(moveRight))
+        {
+            mover.MoveRight(speed);
+        }
+        if (Input.GetKeyDown(jump))
+        {
+            mover.Jump(jumpForce);
+        }
+        if(Input.GetKeyDown(hit))
+        {
+            mover.Hit();
+        }
+    }
+    // to check if player is grouned ( will need to program in later
+    
+}
---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    private Transform tf;
+    private Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        tf = GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void MoveRight(float speed)
+    {
+        rb.AddForce(tf.right * speed);
+    }
+    public void MoveLeft(float speed)
+    {
+        rb.AddForce(tf.right * -speed);
+    }
+    public void Jump(float jumpForce)
+    {
+        rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+    public void Hit()
+    {
+
+    }
+}
+
+
