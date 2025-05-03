1342060ad106ad1955228131c951d3b4c1602b16
gregbufano <113220067+gregbufano@users.noreply.github.com>
2025-05-03 04:19:11 -0700

    Fixed Camera Issues
    Camera now follows the player throughout the different camera breakpoints

--- Assets/MapTransition.cs
+++ Assets/MapTransition.cs
@@ -5,6 +5,15 @@ public class MapTransition : MonoBehaviour
 {
     public PolygonCollider2D mapBoundry;
     public CinemachineConfiner2D confiner;
+    public Direction direction;
+    public float push; // The amount to push the player in the direction of the transition
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,11 +29,39 @@ public class MapTransition : MonoBehaviour
     {
         
     }
+    public void UpdateCameraBoundry(PolygonCollider2D newMapBoundry)
+    {
+        confiner.BoundingShape2D = newMapBoundry;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
             confiner.BoundingShape2D = mapBoundry;
+            UpdatePlayerPosition(collision.gameObject);
+            confiner.InvalidateBoundingShapeCache();
         }
     }
+    private void UpdatePlayerPosition(GameObject player)
+    {
+        Vector3 playerpos = player.transform.position;
+
+        switch (direction)
+        {
+            case Direction.Up:
+                playerpos.y += push;
+                break;
+            case Direction.Down:
+                playerpos.y -= push;
+                break;
+            case Direction.Left:
+                playerpos.x -= push;
+                break;
+            case Direction.Right:
+                playerpos.x += push;
+                break;
+        }
+        player.transform.position = playerpos;
+    }
+
 }
